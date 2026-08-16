521d4558794a681b30b85517b3ef643b4abe4a28
Igwe Miracle <igwemiracle35@gmail.com>
2026-08-16 19:50:25 +0100

    This file contains the signup page component.

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,6 @@
 import { SignupForm } from "@/components/signup-form";
 
+
 export default function SignupPage() {
   return (
     <div className="flex min-h-screen items-center justify-center p-4">
